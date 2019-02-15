94e8de18799c00f9041ec7dafd8ebf5d5ba376ed
Heiko Blobner <h.blobner@avm.de>
2019-02-15 18:21:08 +0100

    enumeratum integration started

---
+++ main/src/test/scala/de/pfke/squeeze/squeezer/annots/injectType/Squeezer_caseClass_w_annotInjectType_spec.scala
@@ -0,0 +1,49 @@
+package de.pfke.squeeze.squeezer.annots.injectType
+
+import java.nio.ByteOrder
+
+import de.pfke.squeeze.Squeezer
+import de.pfke.squeeze.annots.fieldAnnots.injectType
+import de.pfke.squeeze.squeezer.BaseSqueezerSpec
+import de.pfke.squeeze.squeezer.annots.withFixedSize.Squeezer_caseClass_w_annotWithFixedSize_onInvalid_spec.caseClass_w_annotWithFixedSize_onInvalid
+
+object Squeezer_caseClass_w_annotInjectType_spec {
+  case class caseClass_w_annotInjectType(
+    param1: Long,
+    @injectType(fromField = "param3")
+    param2: Byte,
+    param3: String
+  )
+}
+
+class Squeezer_caseClass_w_annotInjectType_spec
+  extends BaseSqueezerSpec {
+    private val inPojo_invalid = caseClass_w_annotWithFixedSize_onInvalid(
+      param1 = 17433124l,
+      param2 = 23,
+    )
+
+    "[simpleSub_invalid] using w/ big endian byte order (an exception should be thrown)" when {
+      implicit val byteOrderToUse: ByteOrder = ByteOrder.BIG_ENDIAN
+
+      "call die scheisse" should {
+        "an exception should be thrown" in {
+          an[IllegalArgumentException] shouldBe thrownBy(
+            Squeezer().toBinary(in = inPojo_invalid)
+          )
+        }
+      }
+    }
+
+    "[simpleSub_invalid] using w/ little endian byte order (an exception should be thrown)" when {
+      implicit val byteOrderToUse: ByteOrder = ByteOrder.LITTLE_ENDIAN
+
+      "call die scheisse" should {
+        "an exception should be thrown" in {
+          an[IllegalArgumentException] shouldBe thrownBy(
+            Squeezer().toBinary(in = inPojo_invalid)
+          )
+        }
+      }
+    }
+  }
